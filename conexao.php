f409da32ea61328770b24574577e0fadc0ebd328
Vitor Ruis <108823068+vitorwhois@users.noreply.github.com>
2023-11-01 18:11:40 -0300

    login e cadastro php

---
+++ conexao.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+
+$usuario = 'root';
+$senha = '';
+$database = 'petpronto';
+$hostname = 'localhost';
+
+$conn = new mysqli($hostname, $usuario, $senha, $database);
+
+if ($conn->error) {
+    die("falha ao conectar ao bancco de dados: " . $conn->error);
+} else
+    echo "Conectado ao Banco de dados ";
