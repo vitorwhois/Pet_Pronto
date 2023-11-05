e9feb7efdfb9d7209ae4e1e735015c26ff95d14c
Vitor Ruis <108823068+vitorwhois@users.noreply.github.com>
2023-11-05 14:00:01 -0300

    update

--- conexao.php
+++ conexao.php
@@ -10,6 +10,6 @@ $hostname = 'localhost';
 $conn = new mysqli($hostname, $usuario, $senha, $database);
 
 if ($conn->error) {
-    die("falha ao conectar ao bancco de dados: " . $conn->error);
+    die('falha ao conectar ao bancco de dados: ' . $conn->error);
 } else
-    echo "Conectado ao Banco de dados ";
+    echo 'Conectado ao Banco de dados ';
